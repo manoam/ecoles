b103d41d57f7e9abf1b524070a6baa217f6a67eb
Ranto Andrianavonison <ranto@tyarai.com>
2017-09-19 09:49:04 +0300

    nanao features sy hanao deploy

--- sites/all/modules/custom/wrappers_custom/includes/taxonomy_term/NiveauTaxonomyTermWrapper.php
+++ sites/all/modules/custom/wrappers_custom/includes/taxonomy_term/NiveauTaxonomyTermWrapper.php
@@ -1,46 +1,66 @@
 <?php
+
 /**
  * @file
  * class NiveauTaxonomyTermWrapper
  */
-
 class NiveauTaxonomyTermWrapper extends WdTaxonomyTermWrapper {
 
-  protected $entity_type = 'taxonomy_term';
-  private static $bundle = 'niveau';
+    protected $entity_type = 'taxonomy_term';
+    private static $bundle = 'niveau';
 
-  /**
-   * Create a new niveau taxonomy_term.
-   *
-   * @param array $values
-   * @param string $language
-   * @return NiveauTaxonomyTermWrapper
-   */
-  public static function create($values = array(), $language = LANGUAGE_NONE) {
-    $values += array('entity_type' => 'taxonomy_term', 'bundle' => self::$bundle, 'type' => self::$bundle);
-    $entity_wrapper = parent::create($values, $language);
-    return new NiveauTaxonomyTermWrapper($entity_wrapper->value());
-  }
+    /**
+     * Create a new niveau taxonomy_term.
+     *
+     * @param array $values
+     * @param string $language
+     * @return NiveauTaxonomyTermWrapper
+     */
+    public static function create($values = array(), $language = LANGUAGE_NONE) {
+        $values += array('entity_type' => 'taxonomy_term', 'bundle' => self::$bundle, 'type' => self::$bundle);
+        $entity_wrapper = parent::create($values, $language);
+        return new NiveauTaxonomyTermWrapper($entity_wrapper->value());
+    }
 
-  /**
-   * Sets field_code
-   *
-   * @param $value
-   *
-   * @return $this
-   */
-  public function setCode($value, $format = NULL) {
-    $this->setText('field_code', $value, $format);
-    return $this;
-  }
+    /**
+     * Sets field_code
+     *
+     * @param $value
+     *
+     * @return $this
+     */
+    public function setCode($value, $format = NULL) {
+        $this->setText('field_code', $value, $format);
+        return $this;
+    }
 
-  /**
-   * Retrieves field_code
-   *
-   * @return mixed
-   */
-  public function getCode($format = WdEntityWrapper::FORMAT_DEFAULT, $markup_format = NULL) {
-    return $this->getText('field_code', $format, $markup_format);
-  }
+    /**
+     * Retrieves field_code
+     *
+     * @return mixed
+     */
+    public function getCode($format = WdEntityWrapper::FORMAT_DEFAULT, $markup_format = NULL) {
+        return $this->getText('field_code', $format, $markup_format);
+    }
+    
+    public static function getNiveauByCode($code) {
+        
+        if($code != NULL){
+            
+            $query = new EntityFieldQuery();
+            $query->entityCondition('entity_type', 'taxonomy_term')
+            ->entityCondition('bundle', 'niveau')
+            ->fieldCondition('field_code', 'value', $code, '=');
+
+            $tids = $query->execute();
+            if (isset($tids['taxonomy_term'])) {
+                $tids = array_keys($tids['taxonomy_term']);
+                foreach ($tids as $tid){
+                    return $tid;
+                }
+            }
+        }
+        return NULL;
+    }
 
 }
