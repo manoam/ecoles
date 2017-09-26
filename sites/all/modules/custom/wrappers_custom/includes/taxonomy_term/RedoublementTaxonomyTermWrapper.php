21364a0c0d8b1453f442983dd5b2c2342b02ed2e
Ranto Andrianavonison <ranto@tyarai.com>
2017-09-26 13:36:17 +0300

    nampiasa ilay taxonomy redoublement

--- sites/all/modules/custom/wrappers_custom/includes/taxonomy_term/RedoublementTaxonomyTermWrapper.php
+++ sites/all/modules/custom/wrappers_custom/includes/taxonomy_term/RedoublementTaxonomyTermWrapper.php
@@ -1,25 +1,65 @@
 <?php
+
 /**
  * @file
  * class RedoublementTaxonomyTermWrapper
  */
-
 class RedoublementTaxonomyTermWrapper extends WdTaxonomyTermWrapper {
 
-  protected $entity_type = 'taxonomy_term';
-  private static $bundle = 'redoublement';
+    protected $entity_type = 'taxonomy_term';
+    private static $bundle = 'redoublement';
 
-  /**
-   * Create a new redoublement taxonomy_term.
-   *
-   * @param array $values
-   * @param string $language
-   * @return RedoublementTaxonomyTermWrapper
-   */
-  public static function create($values = array(), $language = LANGUAGE_NONE) {
-    $values += array('entity_type' => 'taxonomy_term', 'bundle' => self::$bundle, 'type' => self::$bundle);
-    $entity_wrapper = parent::create($values, $language);
-    return new RedoublementTaxonomyTermWrapper($entity_wrapper->value());
-  }
+    /**
+     * Create a new redoublement taxonomy_term.
+     *
+     * @param array $values
+     * @param string $language
+     * @return RedoublementTaxonomyTermWrapper
+     */
+    public static function create($values = array(), $language = LANGUAGE_NONE) {
+        $values += array('entity_type' => 'taxonomy_term', 'bundle' => self::$bundle, 'type' => self::$bundle);
+        $entity_wrapper = parent::create($values, $language);
+        return new RedoublementTaxonomyTermWrapper($entity_wrapper->value());
+    }
 
+    /**
+     * Sets field_code
+     *
+     * @param $value
+     *
+     * @return $this
+     */
+    public function setCode($value, $format = NULL) {
+        $this->setText('field_code', $value, $format);
+        return $this;
+    }
+
+    /**
+     * Retrieves field_code
+     *
+     * @return mixed
+     */
+    public function getCode($format = WdEntityWrapper::FORMAT_DEFAULT, $markup_format = NULL) {
+        return $this->getText('field_code', $format, $markup_format);
+    }
+
+    public static function getByCode($code) {
+
+        if ($code != NULL) {
+
+            $query = new EntityFieldQuery();
+            $query->entityCondition('entity_type', 'taxonomy_term')
+                    ->entityCondition('bundle', 'redoublement')
+                    ->fieldCondition('field_code', 'value', $code, '=');
+
+            $tids = $query->execute();
+            if (isset($tids['taxonomy_term'])) {
+                $tids = array_keys($tids['taxonomy_term']);
+                foreach ($tids as $tid) {
+                    return $tid;
+                }
+            }
+        }
+        return NULL;
+    }
 }
--- sites/all/modules/custom/wrappers_custom/includes/taxonomy_term/RedoublementTaxonomyTermWrapperQuery.php
+++ sites/all/modules/custom/wrappers_custom/includes/taxonomy_term/RedoublementTaxonomyTermWrapperQuery.php
@@ -42,4 +42,27 @@ class RedoublementTaxonomyTermWrapperQuery extends WdTaxonomyTermWrapperQuery {
     return new RedoublementTaxonomyTermWrapperQueryResults($this->entityType, $this->query->execute());
   }
 
+  /**
+   * Query by field_code
+   *
+   * @param mixed $field_code
+   * @param string $operator
+   *
+   * @return $this
+   */
+  public function byCode($field_code, $operator = NULL) {
+    return $this->byFieldConditions(array('field_code' => array($field_code, $operator)));
+  }
+
+  /**
+   * Order by field_code
+   *
+   * @param string $direction
+   *
+   * @return $this
+   */
+  public function orderByCode($direction = 'ASC') {
+    return $this->orderByField('field_code.value', $direction);
+  }
+
 }
